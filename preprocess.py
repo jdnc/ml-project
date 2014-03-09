92f6857e703d6d856250646b898b7294b22febdc
Madhura Parikh <madhuraparikh@gmail.com>
2014-03-09 11:16:37 -0500

    added func to check coordinate validity

--- preprocess.py
+++ preprocess.py
@@ -6,6 +6,8 @@ from collections import defaultdict
 from neurosynth.base.dataset import Dataset:
 import neurosynth.base.imageutils as nbi
 import neurosynth.base.mask as nbm
+import neurosynth.base.transformations as nbt
+
 
 def extract_coordinates(filename):
     """
@@ -29,12 +31,13 @@ def extract_coordinates(filename):
         x = row['x']
         y = row['y']
         z = row['z']
-        doc_dict[row['ID']] .append([x,y,z])
+        if is_valid([x,y,z]):
+            doc_dict[row['ID']].append([x,y,z])
 
     return doc_dict
 
 
-def validate_coordinates(coordinates, mask):
+def is_valid(coordinates, mask='2mm_brain_mask.npy'):
     """
     Validates that the given x/y/z tuple is valid
     for the given mask
@@ -49,10 +52,15 @@ def validate_coordinates(coordinates, mask):
     -------
     bool : whether it is a valid coordinate or not
     """
-    pass
+    mat = np.load(mask)
+    idx = nbt.xyz_to_mat(np.asarray([coordinates]))
+    idx_t = tuple(idx.reshape(1, -1)[0])
+    return mat[idx_t] == 1
 
 
-def peaks_to_vector(coordinates, mask, radius=6):
+def peaks_to_vector(coordinates, mask=
+                    'neurosynth/neurosynth/resourcesneurosynth/neurosynth/resources', 
+                    radius=6):
     """
     Takes in a list of valid peak coordinates and 
     returns a vector of the corresponding image
@@ -98,12 +106,16 @@ def set_targets(filename, threshold=0):
     -------
     target_dict : `collections.defaultdict`
         the key is the study and the value is the target vector
+    target_names : list
+        the name of the target labels used
     """
     target_dict = defaultdict(list)
     feature_table =  pandas.read_table('neurosynth/data/features.txt')
+    target_names = feature_table.columns[1:]
     for idx, row in feature_table.iterrows():
         target_dict[row['Doi']] = [int(x > threshold) for x in row[1:]]
-    return target_dict
+    return (target_dict, target_names)
 
 
-
+def get_features_targets():
+    pass

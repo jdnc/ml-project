75ed3da595acb83751dddbc245c0be301738253f
Madhura Parikh <madhuraparikh@gmail.com>
2014-03-08 19:10:21 -0600

    added function to get target vectors corresponding to each document

--- preprocess.py
+++ preprocess.py
@@ -34,4 +34,47 @@ def extract_coordinates(filename):
     return doc_dict
 
 
+def validate_coordinates(coordinates, mask):
+    """
+    Validates that the given x/y/z tuple is valid
+    for the given mask
+
+    Parameters
+    ----------
+    coordinates : list/tuple
+        x, y, z coordinates to be validated
+    mask : mask object in nifti format
+
+    Returns
+    -------
+    bool : whether it is a valid coordinate or not
+    """
+    pass
+
+    
+def set_targets(filename, threshold=0):
+    """
+    Given the feature file, return the target vector showing 
+    the presence/absence of terms for a document
+    
+    Parameters
+    ----------
+    filename : str
+        complete path to file that has the raw data
+    threshold : real, optional
+        mark term only if its frequency > threshold. Defaults
+        to 0
+
+    Returns
+    -------
+    target_dict : `collections.defaultdict`
+        the key is the study and the value is the target vector
+    """
+    target_dict = defaultdict(list)
+    feature_table =  pandas.read_table('neurosynth/data/features.txt')
+    for idx, row in feature_table.iterrows():
+        target_dict[row['Doi']] = [int(x > threshold) for x in row[1:]]
+    return target_dict
+
+
 

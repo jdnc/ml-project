0084e4f2f01c010ad5854fa0bbff6c307ea5e4ca
Madhura Parikh <madhuraparikh@gmail.com>
2014-03-21 20:36:40 -0500

    basic file for NB classifier

---
+++ naive_bayes.py
@@ -0,0 +1,35 @@
+"""
+Python code to replicate the Naive Bayes classifier from the Large Scale Image
+segmentation paper
+
+Uses Naive Bayes for multi-class OvO classification given 24 labels
+Uses 10-fold cross validation
+"""
+
+from __future__ import print_function
+
+import os
+
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn import cross_validation
+
+
+import preprocess as pp
+import experiment as ex
+
+def get_X_y():
+    coordinate_dict = ex.filter_studies_active_voxels()
+    target_dict = ex.filter_studies_terms(set_unique_label=True)
+    coordinate_dict, target_dict = ex.get_intersecting_dicts(coordinate_dict,
+                                                          target_dict)
+    X, y = pp.get_features_targets(coordinate_dict, target_dict)
+    return X, y
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()

4f55eb82f93ad65db74e8fdfa5c0c4b266f47592
Madhura Parikh <madhuraparikh@gmail.com>
2014-03-29 18:53:12 -0500

    some changes to naive bayes

--- naive_bayes.py
+++ naive_bayes.py
@@ -16,7 +16,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.multiclass import OneVsOneClassifier
 from sklearn import cross_validation
 from sklearn import preprocessing
-
+sklearn.metrics import confusion_matrix
 
 import preprocess as pp
 import experiment as ex
@@ -38,12 +38,12 @@ def main():
     y_enc = le.transform(y)
     # since study assumes uniform prior for each term, set fit_prior to false
     clf = MultinomialNB(fit_prior=False)
-    scores = cross_validation.cross_val_score(OneVsOneClassifier(clf), X,
-                                              y_enc, cv=10)
-    # print the scores
-    print("Scores")
-    print(scores)
-    np.save("/scratch/02863/mparikh/data/scores.npy", scores)
+    kf = cross_validation.KFold(len(y_enc), n_folds=10)
+    conf_mat = np.zeros((len(le.classes_),len(le.classes_)))
+    for train, test in kf:
+        predicted = OneVsOneClassifier(clf).fit(x[train],y_enc[train]).predict(x[test])   
+        conf_mat += confusion_matrix(y_enc[test], predicted)
+    np.save("/scratch/02863/mparikh/data/scores.npy", conf_mat)
 
 
 

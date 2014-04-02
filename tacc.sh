169e9a663111ffb583f2b245b6f828109581baaa
Madhura Parikh <mparikh@login4.ls4.tacc.utexas.edu>
2014-04-01 22:59:17 -0500

    minor option changes

--- tacc.sh
+++ tacc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash	   
 #$ -M mparikh@cs.utexas.edu
+#$ -N NaiveBayes
+#$ -o naive_bayes.out
 module load pylauncher
 python /home1/02863/mparikh/ml-project/naive_bayes.py -s /scratch/02863/mparikh/data/conf.npy

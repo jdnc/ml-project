f565698faedd64167f867ab0b49004227166f495
Madhura Parikh <madhuraparikh@gmail.com>
2014-04-19 20:18:31 -0500

    all keys must be ints

--- experiment.py
+++ experiment.py
@@ -40,7 +40,7 @@ def filter_studies_active_voxels(study_dict, mask, threshold=5000, radius=10):
             if num_activated < threshold:
                 del(study_dict[key])
             else:
-                study_dict[key] = voxels
+                study_dict[len(key)] = voxels
     return study_dict
 
 def filter_studies_terms(feature_file, terms=None, threshold=0.001,

da050ac5d8dd93f52ec2ddac5a96c523b7f1eb6e
Madhura Parikh <madhuraparikh@gmail.com>
2014-04-19 19:43:22 -0500

    all keys are ints

--- preprocess.py
+++ preprocess.py
@@ -37,7 +37,7 @@ def extract_coordinates(filename, mask):
         y = row['y']
         z = row['z']
         if is_valid([x,y,z], mask):
-            doc_dict[row['id']].append([x,y,z])
+            doc_dict[int(row['id'])].append([x,y,z])
     for key in list(doc_dict.keys()):
         if not doc_dict[key]:
             del(doc_dict[key])

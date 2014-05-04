56420a99956c900775e947d3382e047f2a0c3fda
vsubhashini <subhashini87shiny@gmail.com>
2014-05-03 20:18:28 -0500

    Multilabel NB added. Logistic better.

--- multilabel_nb.py
+++ multilabel_nb.py
@@ -3,49 +3,24 @@ from __future__ import print_function
 """
 Multi-label classifier for neurosynth.
 Initially begins with the 25 terms from the paper
-Use k-nearest neighbours to classify.
+Uses Logistic regression for multi-label classification and l1 regularization.
 """
 
 
 import numpy as np
+import random
 import json
 import pickle
 from sklearn import cross_validation
 from sklearn import preprocessing
-from sklearn.neighbors import NearestNeighbors
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.naive_bayes import MultinomialNB
 
 import preprocess as pp
 import experiment as ex
 import utils
 
 THRESH = 0.001
-k = 2 # number of neighbors
-
-def get_predictions(knn_neighbors, y_new):
-  """Compute probability of each label based on frequency of neighbor labels
-     Input: Neighbours, y- the labels (not binarized)
-  """
-  predictions=None #np.zeros((len(knn_neighbors),len(y_new[0])))
-  #predicted_prob_3 = [y_new[neighbors[0]]+y_new[neighbors[1]]+y_new[neighbors[2]] for neighbors in knn_neighbors]
-  predicted_proba=None #np.zeros((len(knn_neighbors),len(y_new[0])), dtype=np.float)
-  for neighbors in knn_neighbors:
-    predicted_labs=np.zeros(len(y_new[0]), dtype=int)
-    for i in range(0,k):
-      predicted_labs+=y_new[neighbors[i]]
-    if(predicted_proba is None):
-      #predicted_proba.append(predicted_labs/(k*1.0))
-      predicted_proba=np.array([predicted_labs/(k*1.0)])
-    else:
-      predicted_proba=np.concatenate((predicted_proba,[predicted_labs/(k*1.0)]))
-    label_ones = predicted_labs >= 1
-    predicted_labs[label_ones]=1
-    if predictions is None:
-      predictions=np.array([predicted_labs])
-    else:
-      #predictions.append(predicted_labs)
-      predictions=np.concatenate((predictions,[predicted_labs]))
-  return (predictions, predicted_proba)
-
 def main():
     feature_dict, col_names = pp.set_targets('data/features.txt', threshold=-1)
     # consider only the terms of interest
@@ -65,8 +40,10 @@ def main():
             del(coord_dict[key])
     # find intersecting dicts
     coord_dict, feature_dict = ex.get_intersecting_dicts(coord_dict, feature_dict)
-    #try for a sample
-    sub_coord_dict = {k: coord_dict[k] for k in coord_dict.keys()[0:3500]}
+    # try for a sample
+    sample4k_coords = random.sample(range(len(coord_dict)), 4000)
+    subsample_keys = [coord_dict.keys()[i] for i in sample4k_coords]
+    sub_coord_dict = {key: coord_dict[key] for key in subsample_keys}
     sub_coord_dict, sub_feature_dict = ex.get_intersecting_dicts(sub_coord_dict, feature_dict)
     X, y = pp.get_features_targets(sub_coord_dict, sub_feature_dict, labels=terms, mask='data/MNI152_T1_2mm_brain.nii.gz')
     # get the respective vectors
@@ -79,13 +56,13 @@ def main():
     score_per_label = []
     predicted_labels=[]
     test_vals=[]
-    knnModel = NearestNeighbors(n_neighbors=k)
+    clf =  OneVsRestClassifier(MultinomialNB())
     kf = cross_validation.KFold(len(y_new), n_folds=10)
     cvrun=1
     for train, test in kf:
-        model = knnModel.fit(X[train])
-        ng_dist, ng_indices  = knnModel.kneighbors(X[test])
-	(predicted, predict_prob) = get_predictions(ng_indices, y_new[train])
+        model = clf.fit(X[train], y_new[train])
+        predict_prob = model.predict_proba(X[test])
+        predicted  = model.predict(X[test])
         cls_scores = utils.score_results(y_new[test], predicted, predict_prob)
         label_scores = utils.label_scores(y_new[test], predicted, predict_prob)
         score_per_class.append(cls_scores)
@@ -95,9 +72,9 @@ def main():
         cvrun+=1
         #if(cvrun>1):
 	#  break
-    with open('knn_2_class_scores.json', 'wb') as f:
+    with open('nbcv10_class_scores.json', 'wb') as f:
         json.dump(score_per_class, f)
-    pickle.dump(score_per_label, open('knn_2_label_scores.p', 'wb'))
+    pickle.dump(score_per_label, open('nbcv10_label_scores.p', 'wb'))
     return
 
 

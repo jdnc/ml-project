61dce314dd3ad59ba10e7378a72404018eaaf504
Madhura Parikh <mparikh@login4.ls4.tacc.utexas.edu>
2014-05-12 17:09:41 -0500

    add back ward clustering

--- tree_multi.py
+++ tree_multi.py
@@ -30,13 +30,13 @@ def classify(x, y):
     # fit a label binarizer
     lb = preprocessing.LabelBinarizer()
     y_new = lb.fit_transform(y)
-    # specify connectivity for clustering
-    #mask = nb.load('data/MNI152_T1_2mm_brain.nii.gz').get_data().astype('bool')
-    #shape = mask.shape
-    #connectivity = image.grid_to_graph(n_x=shape[0], n_y=shape[1], n_z=shape[2], mask=mask)
-    #ward = WardAgglomeration(n_clusters=N_CLUSTERS, connectivity=connectivity)
+    #specify connectivity for clustering
+    mask = nb.load('data/MNI152_T1_2mm_brain.nii.gz').get_data().astype('bool')
+    shape = mask.shape
+    connectivity = image.grid_to_graph(n_x=shape[0], n_y=shape[1], n_z=shape[2], mask=mask)
+    ward = WardAgglomeration(n_clusters=N_CLUSTERS, connectivity=connectivity)
     clf = tree.DecisionTreeClassifier()
-    kf = cross_validation.StratifiedKFold(len(y_new), n_folds=5)
+    kf = cross_validation.KFold(len(y_new), n_folds=5)
     score_per_class = []
     score_per_label = []
     for train, test in kf:
@@ -44,12 +44,12 @@ def classify(x, y):
         train_labels = np.ascontiguousarray(y_new[train])
         test_feat = np.ascontiguousarray(x[test])
         test_labels = np.ascontiguousarray(y_new[test])
-        #ward.fit(train_feat)
-        #train_reduced = ward.transform(train_feat)
-        #test_reduced = ward.transform(test_feat)
-        model = clf.fit(train_feat, train_labels)
-        predicted  = model.predict(test_feat)
-        predict_prob = model.predict_proba(test_feat)
+        ward.fit(train_feat)
+        train_reduced = ward.transform(train_feat)
+        test_reduced = ward.transform(test_feat)
+        model = clf.fit(train_reduced, train_labels)
+        predicted  = model.predict(test_reduced)
+        predict_prob = model.predict_proba(test_reduced)
         cls_scores = utils.score_results(test_labels, predicted, predict_prob)
         label_scores = utils.label_scores(test_labels, predicted, predict_prob)
         score_per_class.append(cls_scores)

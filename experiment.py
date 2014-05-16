b9a7b9f716e0d8856ebd0f6fb33c3a8bb53deb82
Madhura <madhuraparikh@gmail.com>
2014-05-16 04:45:44 -0500

    now the real update

--- experiment.py
+++ experiment.py
@@ -5,7 +5,7 @@ import json
 
 import preprocess as pp
 
-def filter_studies_active_voxels(study_dict, mask, threshold=500, radius=6):
+def filter_studies_active_voxels(study_dict, mask, threshold=5000, radius=10):
     """
     Takes the studies with coordinates, and considers only those studies that
     have number of activated voxels >= threshold. Returns the dict containing
@@ -13,8 +13,8 @@ def filter_studies_active_voxels(study_dict, mask, threshold=500, radius=6):
 
     Parameters
     ----------
-    study_dict  :  dict/str 
-	      filename  to json stored dict or the dict of all the studies with coordinates.     
+    study_dict  :  dict/str
+	      filename  to json stored dict or the dict of all the studies with coordinates.
     mask : mask in nifti format
     threshold : int, optional
         the number of activated voxels in the study, for it to be included.
@@ -34,11 +34,13 @@ def filter_studies_active_voxels(study_dict, mask, threshold=500, radius=6):
     for key in list(study_dict.keys()):
         if len(study_dict[key]) < 0: # study has fewer than 4 reported foci
             del(study_dict[key])
-        else:  # study has less than 5000 activated foci 
+        else:  # study has less than 5000 activated foci
             voxels = pp.peaks_to_vector(study_dict[key], mask, radius=radius)
             num_activated = (voxels > 0).sum()
             if num_activated < threshold:
                 del(study_dict[key])
+            else:
+                study_dict[len(key)] = voxels
     return study_dict
 
 def filter_studies_terms(feature_file, terms=None, threshold=0.001,
@@ -52,7 +54,7 @@ def filter_studies_terms(feature_file, terms=None, threshold=0.001,
     ----------
     feature_file : str
         the file with the raw features.
-    terms : list of str, optional 
+    terms : list of str, optional
         the terms that are being considered as labels. If not specified,
         uses the 25 terms from the original study.
     threshold : real, optional
@@ -96,7 +98,7 @@ def filter_studies_terms(feature_file, terms=None, threshold=0.001,
                 'Recognition'
                 ]
     feature_dict, target_names = pp.set_targets(feature_file,
-                                                    threshold=-1)   
+                                                    threshold=-1)
     # validate that the terms are actual features and convert to lower case
     new_terms = [x.lower() for x in terms if x.lower() in target_names]
     for key in list(feature_dict.keys()):
@@ -117,13 +119,13 @@ def filter_studies_terms(feature_file, terms=None, threshold=0.001,
             else:
                 del(feature_dict[key])
     return feature_dict
-    
+
 def get_intersecting_dicts(coordinate_dict, target_dict):
     """
     Given dicts with studies, filtered on diffierent criteria, convenience
     function that returns dicts having only the studies that pass all the
     criteria
-    
+
     Parameters
     ----------
     coordinate_dict :  dict
@@ -138,7 +140,7 @@ def get_intersecting_dicts(coordinate_dict, target_dict):
     """
     new_coordinate_dict = {}
     new_target_dict = {}
-    intersection = [key for key in coordinate_dict if key in target_dict]
+    intersection = [key for key in target_dict if int(key) in coordinate_dict]
     for key in intersection:
         new_coordinate_dict[key] = coordinate_dict[key]
         new_target_dict[key] = target_dict[key]

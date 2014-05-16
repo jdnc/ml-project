3d5dadf7daf75c877d4f2f583b6992b963a57696
Madhura Parikh <madhuraparikh@gmail.com>
2014-05-16 05:31:33 -0500

    deleted useless clutter

--- vsub_tacc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash	   
-#$ -M vsub@cs.utexas.edu
-#$ -N NaiveBayes
-#$ -o naive_bayes.out
-module load pylauncher
-#python /home1/02869/vsub/ml-project/multi-check.py
-python /home1/02869/vsub/ml-project/naive_bayes.py -c /scratch/02869/vsub/data/docdict.txt  -f /scratch/02869/vsub/data/features.txt -s $SCRATCH/ 
